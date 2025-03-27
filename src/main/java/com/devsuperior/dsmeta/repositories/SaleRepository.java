0acabc8d6da381ec9cdd505e999fb938ef22f233
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2025-03-26 23:57:39 -0300

    Create endpoint summary.

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.dsmeta.repositories;
 
+import com.devsuperior.dsmeta.dto.SellerTotalDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SellerTotalDTO(obj.name, SUM(sa.amount)) FROM Seller obj JOIN obj.sales sa " +
+            "WHERE (:name = '' OR UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))) " +
+            "AND sa.date BETWEEN :minDate AND :maxDate " +
+            "GROUP BY obj.name")
+    List<SellerTotalDTO> searchSellerTotalSales(LocalDate maxDate, LocalDate minDate, String name);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,7 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SellerTotalDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +24,24 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public List<SellerTotalDTO> findSummary(String minDate, String maxDate, String name) {
+		LocalDate min;
+		LocalDate max;
+
+		if (maxDate != null) {
+			max = LocalDate.parse(maxDate);
+		} else {
+			max = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}
+
+		if (minDate != null) {
+			min = LocalDate.parse(minDate);
+		} else {
+			min = max.minusYears(1L);
+		}
+
+		return repository.searchSellerTotalSales(max, min, name);
 	}
 }
